bcffb9a47f103628737646454800db8a633c60ab
gdanny94 <109629790+gdanny94@users.noreply.github.com>
2024-09-19 20:12:12 -0400

    typeof Sum Code

---
+++ JS/main.js
@@ -0,0 +1,12 @@
+//Instructions
+
+// Return the type of the sum of the two arguments
+
+
+//Solution
+
+function typeOfSum(a, b) {
+    return typeof (a + b)
+  }
+  
+  console.log(typeOfSum( 1, 2))
